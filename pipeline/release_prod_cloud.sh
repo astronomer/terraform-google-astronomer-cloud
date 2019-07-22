9d2eefbf49e307ef6e3be1d91e4ce6e3ecdf7a7d
Steven Miller <sjmiller609@gmail.com>
2019-07-22 19:11:38 +0000

    WIP: setting up pipeline to deploy to production

--- pipeline/release_prod_cloud.sh
+++ pipeline/release_prod_cloud.sh
@@ -13,7 +13,7 @@ terraform -v
 
 # unique deployment ID to avoid collisions in CI
 # needs to be 32 characters or less and start with letter
-DEPLOYMENT_ID='staging'
+DEPLOYMENT_ID='prod'
 ZONAL='false'
 
 export EXAMPLE='from_scratch'
@@ -22,29 +22,56 @@ cp providers.tf.example examples/$EXAMPLE/providers.tf
 cp backend.tf.example examples/$EXAMPLE/backend.tf
 
 cd examples/$EXAMPLE
+# TODO: swap to prod stuff when we get a new project
 sed -i "s/REPLACE/$DEPLOYMENT_ID/g" backend.tf
 sed -i "s/BUCKET/astronomer-staging-terraform-state/g" backend.tf
 sed -i "s/PROJECT/astronomer-cloud-staging/g" providers.tf
 
 terraform init
 
-# TODO: add to CI image
-apk add jq
+if [[ ${FIRST_RUN:-0} -eq 1 ]]; then
 
-# copy the kubeconfig from the terraform state
+  terraform apply --auto-approve \
+    -var "deployment_id=$DEPLOYMENT_ID" \
+    -var "dns_managed_zone=staging-zone" \
+    -var "zonal=$ZONAL" \
+    -lock=false \
+    -input=false
+
+  exit 0
+
+fi
+
 terraform state pull | jq -r '.resources[] | select(.module == "module.astronomer_cloud") | select(.name == "kubeconfig") | .instances[0].attributes.content' > kubeconfig
 chmod 755 kubeconfig
-
-# whitelist our current IP for kube management API
 gcloud container clusters update steven-cluster --master-authorized-networks="$(curl icanhazip.com)/32" --enable-master-authorized-networks --zone=us-east4
 
-terraform apply --auto-approve \
-  -var "deployment_id=$DEPLOYMENT_ID" \
-  -var "dns_managed_zone=staging-zone" \
-  -var "zonal=$ZONAL" \
-  -var "kubeconfig_path=./kubeconfig" \
-  -lock=false \
-  -input=false
+if [[ ${TF_PLAN:-0} -eq 1 ]]; then
+	terraform plan -detailed-exitcode \
+	  -var "deployment_id=$DEPLOYMENT_ID" \
+	  -var "dns_managed_zone=staging-zone" \
+	  -var "zonal=$ZONAL" \
+    -var "kubeconfig_path=./kubeconfig" \
+	  -lock=false \
+	  -out=tfplan \
+	  -input=false
+fi
+
+if [[ ${TF_APPLY:-0} -eq 1 ]]; then
+
+  # TODO: add to CI image
+  apk add jq
+
+	terraform apply --auto-approve \
+	  -var "deployment_id=$DEPLOYMENT_ID" \
+	  -var "dns_managed_zone=staging-zone" \
+	  -var "zonal=$ZONAL" \
+    -var "kubeconfig_path=./kubeconfig" \
+	  -lock=false \
+	  -refresh=false \
+	  -input=false tfplan
+
+fi
 
 rm providers.tf
 rm backend.tf
