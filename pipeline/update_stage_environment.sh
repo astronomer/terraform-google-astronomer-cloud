49bf87b602b783d9a35c9e1dfb40bec242b96df3
Steven Miller <sjmiller609@gmail.com>
2019-07-26 13:24:47 +0000

    fix auto updates

--- pipeline/update_stage_environment.sh
+++ pipeline/update_stage_environment.sh
@@ -1,13 +1,20 @@
-#!/bin/bash
+#!/bin/sh
+
+apk add sed
 
 cd /tmp
 eval `ssh-agent`
 echo "$DEPLOY_KEY" | ssh-add -
 mkdir -p $HOME/.ssh
+
+set -xe
 ssh-keyscan -t rsa github.com >> $HOME/.ssh/known_hosts
 git clone git@github.com:astronomer/google-environments.git
 cd google-environments
 sed -i "s/version\s=\s\"[0-9]*\.[0-9]*\.[0-9]*\"/version = \"$DRONE_TAG\"/g" stage/cloud/main.tf
 git add stage/cloud/main.tf
+git status
+git config --global user.email "steven@astronomer.io"
+git config --global user.name "Drone CI"
 git commit -m "Drone CI: Update stage cloud with Astronomer chart version $DRONE_TAG"
 git push origin master
