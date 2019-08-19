ffe1a783dd1c0b67061297e4c1d1aa3baf9024e1
sjmiller609 <sjmiller609@gmail.com>
2019-08-19 09:15:57 -0400

    Upgrade to Astronomer 0.10.0-rc.8

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "astronomer" {
   dependencies       = [module.system_components.depended_on]
   source             = "astronomer/astronomer/kubernetes"
   version            = "1.1.20"
-  astronomer_version = "0.10.0-alpha.5"
+  astronomer_version = "0.10.0-rc.8"
 
   db_connection_string = "postgres://${module.gcp.db_connection_user}:${module.gcp.db_connection_password}@pg-sqlproxy-gcloud-sqlproxy.astronomer:5432"
   tls_cert             = var.tls_cert == "" ? module.gcp.tls_cert : var.tls_cert
