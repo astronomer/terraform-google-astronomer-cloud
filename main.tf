fe1bb5070ac8fbdef8a0cd08e246ee0cccd649da
sjmiller609 <sjmiller609@gmail.com>
2019-09-12 14:40:15 -0400

    update to 0.10.1-alpha.8

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "astronomer" {
   module.gcp.depended_on]
   source             = "astronomer/astronomer/kubernetes"
   version            = "1.1.20"
-  astronomer_version = "0.10.1-alpha.7"
+  astronomer_version = "0.10.1-alpha.8"
 
   db_connection_string = "postgres://${module.gcp.db_connection_user}:${module.gcp.db_connection_password}@pg-sqlproxy-gcloud-sqlproxy.astronomer:5432"
   tls_cert             = var.tls_cert == "" ? module.gcp.tls_cert : var.tls_cert
