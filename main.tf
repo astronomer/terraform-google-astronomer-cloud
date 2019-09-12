2ff5921b4e299c0a284f2a70f52c7accb65fccff
Drone CI <steven@astronomer.io>
2019-09-12 20:19:34 +0000

    Drone CI: Update GCP infra module to 1.0.111

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Networks, Database, Kubernetes cluster, etc.
 module "gcp" {
   source              = "astronomer/astronomer-gcp/google"
-  version             = "1.0.109"
+  version             = "1.0.111"
   email               = var.email
   deployment_id       = var.deployment_id
   dns_managed_zone    = var.dns_managed_zone
