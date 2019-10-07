a0e2fb0398d0d46ccbec5f77da6ca427708796e4
Steven Miller <sjmiller609@gmail.com>
2019-10-07 17:51:46 +0000

    NetworkPolicy for Cloud SQL proxy

--- locals.tf
+++ locals.tf
@@ -260,6 +260,22 @@ schedules:
       ttl: "720h"
 EOF
 
+  extra_googlesqlproxy_helm_values = <<EOF
+---
+networkPolicy:
+  enabled: true
+  ingress:
+    from:
+      - podSelector:
+          matchLabels:
+            tier: airflow
+            component: pgbouncer
+      - podSelector:
+          matchLabels:
+            tier: astronomer
+            component: prisma
+EOF
+
   tiller_tolerations = [
     {
       key      = "platform",
