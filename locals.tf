37baeacfdc07eeca18c13d31969970803d64fd96
Steven Miller <sjmiller609@gmail.com>
2019-10-31 16:04:22 +0000

    Add Houston webhook config

--- locals.tf
+++ locals.tf
@@ -173,6 +173,9 @@ alertmanager:
             {{ end }}
           {{ end }}
     airflow:
+      webhook_configs:
+      - url: "http://{{ .Release.Name }}-houston:8871/v1/alerts"
+        send_resolved: true
       slack_configs:
       - channel: "${var.slack_alert_channel}"
         api_url: "${var.slack_alert_url}"
