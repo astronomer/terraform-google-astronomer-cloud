d96314d688656a38b23431badf9b93cbc5304fe1
Steven Miller <sjmiller609@gmail.com>
2019-11-06 17:40:09 +0000

    Drone CI: Update GCP infra module to 1.0.165

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 module "gcp" {
   source = "astronomer/astronomer-gcp/google"
   //  source              = "../terraform-google-astronomer-gcp"
-  version             = "1.0.163"
+  version             = "1.0.165"
   email               = var.email
   deployment_id       = var.deployment_id
   dns_managed_zone    = var.dns_managed_zone
