d79be50bda428aec60d921386e97f70d2f4e9377
sjmiller609 <sjmiller609@gmail.com>
2020-01-08 16:28:14 -0500

    Update Astronomer module

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "system_components" {
 module "astronomer" {
   dependencies       = [module.system_components.depended_on, module.gcp.depended_on]
   source             = "astronomer/astronomer/kubernetes"
-  version            = "1.1.53"
+  version            = "1.1.55"
   astronomer_version = "0.11.0-rc.4"
 
   db_connection_string = "postgres://${module.gcp.db_connection_user}:${module.gcp.db_connection_password}@pg-sqlproxy-gcloud-sqlproxy.astronomer:5432"
