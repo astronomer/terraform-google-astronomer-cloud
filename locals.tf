c65d7f844db946f71d5d1a628cfb292d722e9934
sjmiller609 <sjmiller609@gmail.com>
2020-02-07 10:41:39 -0500

    Add istio-citadel replica (#126)

--- locals.tf
+++ locals.tf
@@ -442,7 +442,11 @@ global:
               echo "Network connections cleared, shutting down pilot..."
               exit 0
     # https://github.com/istio/istio/issues/8247
-    concurrency: 1
+    # After applying the other fixes for memory,
+    # i.e. sidecar egress rules, this fix is no
+    # longer necessary, and it's better to set back
+    # to the default (2) for performance.
+    concurrency: 2
     # Only use sidecar for RFC1918 address space (private networks).
     # This will allow mesh-external traffic to leave the cluster
     # without going through a sidecar.
@@ -469,6 +473,8 @@ galley:
   replicaCount: 2
 pilot:
   autoscaleMin: 2
+security:
+  replicaCount: 2
 mixer:
   policy:
     autoscaleMin: 2
