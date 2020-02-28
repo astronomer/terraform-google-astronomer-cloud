f6e432e64d2be1f143ba51df49a28707d7c3d499
sjmiller609 <sjmiller609@gmail.com>
2020-02-28 14:53:11 -0500

    Add label to Astronomer namespace

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ module "gcp" {
 module "system_components" {
   dependencies = [module.gcp.depended_on]
   source       = "astronomer/astronomer-system-components/kubernetes"
-  version      = "0.1.18"
+  version      = "0.1.19"
   //  source                       = "../terraform-kubernetes-astronomer-system-components"
   enable_cloud_sql_proxy             = true
   enable_istio                       = var.enable_istio
