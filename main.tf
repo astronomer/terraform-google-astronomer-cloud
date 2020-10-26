89460f2c6cd0935e48970185c908949c40558d74
Jim Weber <jpweber@gmail.com>
2020-10-26 15:39:40 -0400

    correct module I am using for system components

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ module "gcp" {
 module "system_components" {
   dependencies = [module.gcp.depended_on]
 
- source = "github.com/astronomer/terraform-kubernetes-astronomer?ref=gke-channels"
+ source = "github.com/astronomer/terraform-kubernetes-astronomer-system-components"
 
   astronomer_namespace               = var.astronomer_namespace
   enable_cloud_sql_proxy             = var.enable_cloud_sql_proxy
