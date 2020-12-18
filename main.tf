d5d6e60499b2929997e5d89346a709806bd82dd1
Daniel Hoherd <daniel.hoherd@gmail.com>
2020-12-18 11:33:19 -0800

    switch from gke-channels branch to 1.1.0 tag

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Networks, Database, Kubernetes cluster, etc.
 module "gcp" {
 
-  source = "github.com/astronomer/terraform-google-astronomer-gcp?ref=gke-channels"
+  source = "github.com/astronomer/terraform-google-astronomer-gcp?ref=1.1.0"
 
   email                   = var.email
   deployment_id           = var.deployment_id
