8cb729bfd8e1aa6c1c0bea54b16e07e509ecbf63
tommy kelly <tommy@astronomer.io>
2020-12-22 09:58:46 -0500

    pointing to temp gcp branch

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # Networks, Database, Kubernetes cluster, etc.
 module "gcp" {
 
-  source = "github.com/astronomer/terraform-google-astronomer-gcp?ref=1.1.0"
+  source = "github.com/astronomer/terraform-google-astronomer-gcp?ref=add-disk-type-as-parameter"
 
   email                   = var.email
   deployment_id           = var.deployment_id
